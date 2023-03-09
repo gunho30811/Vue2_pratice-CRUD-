65871a41b8245eccd79dbbae79f463564c147d6b
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-09 15:09:54 +0900

    2023-03-09 15:09
    모듈 제거 후 리스트 재생성

--- my_app/src/main.js
+++ my_app/src/main.js
@@ -1,11 +1,7 @@
-import { create } from 'core-js/core/object'
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import router from './router/index.js'
-import 'core-js/core/object';
 
-create(App).use(router).mount('#app')
 Vue.config.productionTip = false
 
 new Vue({
