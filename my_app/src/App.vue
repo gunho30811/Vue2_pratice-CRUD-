d6e3bacaa9b3fbc61daddd5a0b2d51df9638cd18
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-09 14:19:13 +0900

    2023-03-09 14:19
    Todo list

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -1,52 +1,90 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-container fluid>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="6" lg="4">
+          <v-card>
+            <v-toolbar color="primary" dark>
+              <v-toolbar-title>Todo List</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="clearCompleted">
+                <v-icon>mdi-delete-variant</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text>
+              <v-text-field
+                v-model="newTodo"
+                label="New Todo"
+                outlined
+                @keyup.enter="addTodo"
+              ></v-text-field>
+              <v-btn color="primary" @click="addTodo">Add</v-btn>
+              <v-list>
+                <v-list-item-group>
+                  <v-list-item
+                    v-for="(todo, index) in todos"
+                    :key="index"
+                  >
+                    <v-list-item-action>
+                      <v-checkbox
+                        v-model="todo.done"
+                        @change="updateTodo(index)"
+                        color="primary"
+                      ></v-checkbox>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        :class="{ 'text-strike': todo.done }"
+                      >{{ todo.text }}</v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                      <v-btn
+                        icon
+                        @click="removeTodo(index)"
+                        color="error"
+                      >
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { text: 'Learn Vue.js', done: false },
+        { text: 'Learn Vuetify', done: false },
+        { text: 'Build a Todo List', done: true }
+      ]
+    }
   },
-
-  data: () => ({
-    //
-  }),
-};
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim()) {
+        this.todos.push({ text: this.newTodo, done: false })
+        this.newTodo = ''
+      }
+    },
+    updateTodo(index) {
+      this.todos[index].done = !this.todos[index].done
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.done)
+    }
+  }
+}
 </script>
