368550bd0df47702e3973ace27a93498ad1a78ba
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-10 10:36:09 +0900

    2023-03-10 10:35
    라우터 적용, 이제 투두리스트랑
    새로 만들 게시판을 적용시켜서 SPA형태로 라우터 사용할 것.

---
+++ my_app/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HelloWorld",
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    font-size: 3rem;
+    text-align: center;
+    margin-top: 3rem;
+  }
+  </style>
