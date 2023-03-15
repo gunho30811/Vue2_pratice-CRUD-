e0ce8b49a9aaa43a2c80c6d759ae4836e187fd6e
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2023-03-15 09:04:26 +0900

    Update App.vue

--- my_app/src/App.vue
+++ my_app/src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
+    <MyTask v-bind:tasks="tasks"></MyTask>
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <router-link to="/">home</router-link>
+    <router-link to="/about">About</router-link>
     <router-view/>
+  </nav>
   </div>
 </template>
+<script>
+import MyTask from "./components/MyTask";
+import router from "./router";
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  name: "App",
+  components: {
+    MyTask,
+    router
+},
+  data() {
+    return {
+      tasks: [
+      ],
+    };
+  },
+};
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
